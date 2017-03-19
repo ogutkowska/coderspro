016a4f9f00fc0460678406f5e3c47e58405f6977
ogutkowska <o.gutkowskaz@gmail.com>
2017-03-19 12:55:33 +0100

    function as parameters

--- niedziela.js
+++ niedziela.js
@@ -1,53 +1,16 @@
-/*// typy opakowujace
+//funkcja jako wartosc
 
-//string, number, boolean typy referencyjne ktorymi mozna sie poslugiwac jak obiektami
-
-var name = 'Pawel';
-var firstCharacter = name.charAt(0);
-console.log(firstCharacter); // wyswietli pierwsza litere w stringu
-//to co sie dzieje w tle
-var name1 = 'Marcin' ;
-var temporary = new String(name1);
-var firstCharacter1 = temporary.charAt(0);
-temporary = null;
-console.log(firstCharacter1);
-
-//
-var name = "Grzesiek";
-name.last = "Kowalski";
-console.log(name.last); // undefined
-
-var name2 = 'Zosia';
-var temp = new String(name2);
-temp.last = 'Kowalska';
-temp = null;
-var temp = new String(name2);
-console.log(temp.last);//undefinded
-temp = null;
-
-//
-
-var name3 = 'Ola';
-var cout = 10;
-var found = false;
-
-console.log(name3 instanceof String); // false typeof true ale instance of false bo tylko na chwile zmienia instancje ale potem ja niszczy i wraca do typu prostego
-console.log(count instanceof Number); // false
-console.log(found instanceof Boolean); // false*/
-
-
-var name4 = new String ('Ola');
-var count1 = new Number(10);
-var found1 = new Boolean(false);
-
-console.log(typeof name4);
-console.log(typeof count1);
-console.log(typeof found1);
-
-var founded = new Boolean(false); // wyswietli pierwsze bo founded istnieje, wyswietli ze znalazł nie wazne ze argument jest false
-
-if(founded) {
-  console.log('I found a variable!');
-}else {
-  console.log("I haven't found a variable!");
+function sayHello () {
+  console.log('Hi');
 }
+
+sayHello();
+var sayHello2 = sayHello;
+
+var numbers = [1, 45, 2, 4, 6, 78, 11]; //sortuje 1 11 2 3 4 45
+console.log(numbers.sort());
+
+var foo = numbers.sort(function (a, b)) { //sortuje po kolei
+  return a - b;
+});
+console.log(foo);
