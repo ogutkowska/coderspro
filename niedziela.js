16cdd672668422acee8b067495355ec3eb173a19
ogutkowska <o.gutkowskaz@gmail.com>
2017-03-19 13:32:48 +0100

    js overloading

--- niedziela.js
+++ niedziela.js
@@ -1,52 +1,27 @@
-/*//funkcja jako wartosc
+//przeciazenia (overloading)
 
-function sayHello () {
-  console.log('Hi');
+/*function message (mes) {
+  console.log(mes);
+
 }
 
-sayHello();
-var sayHello2 = sayHello;
+function message () {
+  console.log('Default');// wyswietli default bo java wyswietla ostatnia funkcje zapomina o pierwszej wersji
 
-var numbers = [1, 45, 2, 4, 6, 78, 11]; //sortuje 1 11 2 3 4 45
-console.log(numbers.sort());
-
-var foo = numbers.sort(function (a, b)) { //sortuje po kolei
-  return a - b;
-});
-console.log(foo);*/
-
-
-//parametry funkcji
-
-/*function show(value) {
-  return value;
 }
 
-console.log (show('Hi')); //Hi
-console.log (show('Hi', 34)) // Hi
-console.log (show.length); // 1 arnosc funkcji*/
+message ('Hello')*/
 
+/*var message = new Function('message', 'console.log(message)');
 
-/*show = function() {
-  return arguments[0];
-}
-console.log(show('Hello')); // Hello
-console.log(show('Hello', 45));// Hello
-console.log(show.length);// 0*/
+message = new Function('console.log("Default")');
+message('Hello');*/
 
-/*function sum() {
-  var result = 0;
-  var len = arguments.length;
-  var i=0;
-
-  while (i< len) {
-    result += arguments[i];
-    i++;
+function message (message) {
+  if(arguments.length === 0){
+    message = 'Default';
   }
-  return result;
+  console.log(message);
 }
-
-console.log(sum(1,5));//6
-console.log(sum(4,5,7));//16
-console.log(sum(3));//3
-console.log(sum());//0*/
+console.log(message('Hello')); // Hello
+console.log(message()); // Default
