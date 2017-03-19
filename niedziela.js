2a28c96441be8a3f5655270dfb10de280493048d
ogutkowska <o.gutkowskaz@gmail.com>
2017-03-19 10:49:12 +0100

    identifying reference item

--- niedziela.js
+++ niedziela.js
@@ -1,10 +1,22 @@
-//dostep do wlasciwosci/kluczy
+//Identyfikowanie typow referencyjnych
 
-var array = [];
+function add(a,b) {
+  return a + b;
+}
 
-array.push(2345566); //dodawanie do tablicy
+console.log(typeof add);
 
-array['push'] (546353) //inny zapis
+var item = [];
+var obj = {};
 
-var method = 'push'; // rzadko spotykane
-array[method] (5433);
+console.log(item instanceof Array);//true
+console.log(obj instanceof Object);//true
+console.log(add instanceof Function);//true
+
+
+console.log(item instanceof Object); // true bo tablica jest obiektem typy referencyjne dziedzicza po obiekcie
+
+console.log(obj instanceof Array); //false
+console.log (add instanceof Object); //true funkcja dziedziczy po obiekcie
+
+console.log(Array.isArray(item)); // true, nowa metodajasno okresla czy jakis obiekt jest arrayem, dziala tylko dla arrayow
