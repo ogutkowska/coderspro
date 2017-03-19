082eb5b5cbdf6a8be8d80d5c974b98a58350976f
ogutkowska <o.gutkowskaz@gmail.com>
2017-03-19 13:12:55 +0100

    parameters of functions

--- niedziela.js
+++ niedziela.js
@@ -1,4 +1,4 @@
-//funkcja jako wartosc
+/*//funkcja jako wartosc
 
 function sayHello () {
   console.log('Hi');
@@ -13,4 +13,40 @@ console.log(numbers.sort());
 var foo = numbers.sort(function (a, b)) { //sortuje po kolei
   return a - b;
 });
-console.log(foo);
+console.log(foo);*/
+
+
+//parametry funkcji
+
+/*function show(value) {
+  return value;
+}
+
+console.log (show('Hi')); //Hi
+console.log (show('Hi', 34)) // Hi
+console.log (show.length); // 1 arnosc funkcji*/
+
+
+/*show = function() {
+  return arguments[0];
+}
+console.log(show('Hello')); // Hello
+console.log(show('Hello', 45));// Hello
+console.log(show.length);// 0*/
+
+/*function sum() {
+  var result = 0;
+  var len = arguments.length;
+  var i=0;
+
+  while (i< len) {
+    result += arguments[i];
+    i++;
+  }
+  return result;
+}
+
+console.log(sum(1,5));//6
+console.log(sum(4,5,7));//16
+console.log(sum(3));//3
+console.log(sum());//0*/
